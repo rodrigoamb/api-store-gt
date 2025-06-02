01afa7c469bbc16e3fce913e30069574c47d0c80
Rodrigo Medeiros <dev.rodrigomedeiros@gmail.com>
2025-06-02 14:58:59 -0300

    modelcliente criado

---
+++ src/models/clienteModel.js
@@ -0,0 +1,34 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/db";
+
+const Cliente = sequelize.define(
+  "Cliente",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    nome: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      unique: true,
+    },
+    telefone: {
+      type: DataTypes.STRING(15),
+      allowNull: true,
+    },
+  },
+  {
+    tableName: "clientes",
+    timestamps: true,
+    createdAt: "created_at",
+    updatedAt: "updated_at",
+  }
+);
+
+export default Cliente;
