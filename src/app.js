49b38ccdc5beb7ad6a48fb228390d0181375879f
Rodrigo Medeiros <dev.rodrigomedeiros@gmail.com>
2025-06-02 16:10:13 -0300

    atualizado app.js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 import express from "express";
+import router from "./routes/router";
+import sequelize from "./config/db.js";
 
 const app = express();
 
@@ -6,7 +8,14 @@ const app = express();
 app.use(express.json());
 
 //Rotas
+app.use("/api", router);
 
 //Sincronizar as tabelas do banco
+sequelize
+  .sync()
+  .then(() => console.log("Banco de dados sincronizado"))
+  .catch((error) =>
+    console.error("Erro ao sincronizar o banco de dados:", error)
+  );
 
 export default app;
